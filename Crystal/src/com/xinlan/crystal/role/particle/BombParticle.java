feded9c8b77a2480f4b5128c382504818b163245
siwangqishiq <525647740@qq.com>
2014-06-02 19:02:07 +0800

    增加不同颜色方块的粒子效果

--- Crystal/src/com/xinlan/crystal/role/particle/BombParticle.java
+++ Crystal/src/com/xinlan/crystal/role/particle/BombParticle.java
@@ -18,8 +18,13 @@ public class BombParticle {
 	private GameScreen context;
 
 	private ParticleEffect blueParticle;// 蓝粒子
-	private ParticleEffect temBlueParticle;
+	private ParticleEffect pinkParticle;
+	private ParticleEffect redParticle;
+	private ParticleEffect yellowParticle;
 	private ParticleEffectPool bluePool;// 蓝色粒子池 产生蓝色粒子
+	private ParticleEffectPool pinkPool;//粉色池
+	private ParticleEffectPool redPool;//红池
+	private ParticleEffectPool yellowPool;
 
 	private Array<ParticleEffect> particleList = new Array<ParticleEffect>();// 存贮所有粒子的容器
 
@@ -33,9 +38,24 @@ public class BombParticle {
 	 */
 	private void load() {
 		blueParticle = new ParticleEffect();
-		blueParticle.load(Gdx.files.internal("particle/blue_particle.p"),
+		blueParticle.load(Gdx.files.internal("particle/blue"),
 				Gdx.files.internal("particle"));
 		bluePool = new ParticleEffectPool(blueParticle, 10, 15);
+		
+		pinkParticle = new ParticleEffect();
+		pinkParticle.load(Gdx.files.internal("particle/pink"),
+				Gdx.files.internal("particle"));
+		pinkPool = new ParticleEffectPool(pinkParticle, 10, 15);
+		
+		redParticle = new ParticleEffect();
+		redParticle.load(Gdx.files.internal("particle/red"),
+				Gdx.files.internal("particle"));
+		redPool = new ParticleEffectPool(redParticle, 10, 15);
+		
+		yellowParticle = new ParticleEffect();
+		yellowParticle.load(Gdx.files.internal("particle/orange"),
+				Gdx.files.internal("particle"));
+		yellowPool = new ParticleEffectPool(yellowParticle, 10, 15);
 	}
 
 	/**
@@ -75,10 +95,37 @@ public class BombParticle {
 				+ (CoreData.CUBE_WIDTH >> 1);
 		float posY = GameScreen.SC_HEIGHT
 				- (CoreData.CUBE_BORN_Y + CoreData.CUBE_HEIGHT * row - (CoreData.CUBE_HEIGHT >> 1));
-		ParticleEffect addParticle = bluePool.obtain();
-		System.out.println(posX+"     "+posY);
+		ParticleEffect addParticle = selectParticleFromType(type);
 		addParticle.setPosition(posX, posY);
 		particleList.add(addParticle);
+	}
+	
+	/**
+	 * 根据类型 返回相应颜色的粒子效果
+	 * @param type
+	 * @return
+	 */
+	private ParticleEffect selectParticleFromType(int type)
+	{
+		ParticleEffect retParticle = null;
+		switch(type)
+		{
+		case CoreData.BLUE:
+			retParticle = bluePool.obtain();
+			break;
+		case CoreData.PINK:
+			retParticle = pinkPool.obtain();
+			break;
+		case CoreData.RED:
+			retParticle = redPool.obtain();
+			break;
+		case CoreData.YELLOW:
+			retParticle = yellowPool.obtain();
+			break;
+		default:
+			retParticle = bluePool.obtain();
+		}
+		return retParticle;
 	}
 
 	public void dispose() {
